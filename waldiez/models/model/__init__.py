ce4df84a9802afeb4a7890c7b83bbd018195e9c6
Lazaros Toumanidis <laztoum@protonmail.com>
2024-12-18 15:29:48 +0200

    update models: use default base_url if not provided.

--- waldiez/models/model/__init__.py
+++ waldiez/models/model/__init__.py
@@ -1,9 +1,10 @@
 """Waldiez model."""
 
-from .model import WaldiezModel
+from .model import DEFAULT_BASE_URLS, WaldiezModel
 from .model_data import WaldiezModelAPIType, WaldiezModelData, WaldiezModelPrice
 
 __all__ = [
+    "DEFAULT_BASE_URLS",
     "WaldiezModel",
     "WaldiezModelData",
     "WaldiezModelPrice",
